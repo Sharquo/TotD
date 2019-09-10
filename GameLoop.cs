da7818bd146290f4600c621f47dbe5a723151254
Alexander Taxter <sharquo@gmail.com>
2019-09-10 18:00:39 +0100

    Basic console and movement.

---
+++ GameLoop.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using SadConsole;
+using Console = SadConsole.Console;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TotD
+{
+    class GameLoop
+    {
+
+        public const int Width = 80;
+        public const int Height = 25;
+
+        private static SadConsole.Entities.Entity player;
+
+        static void Main(string[] args)
+        {
+            // Setup the engine and create the main window.
+            SadConsole.Game.Create(Width, Height);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Game.OnInitialize = Init;
+
+            // Hook the update event that happens each frame so we can trap keys and respond.
+            SadConsole.Game.OnUpdate = Update;
+
+            // Start the game.
+            SadConsole.Game.Instance.Run();
+
+            //
+            // Code here will not run until the game window closes.
+            //
+
+            SadConsole.Game.Instance.Dispose();
+        }
+
+        private static void Update(GameTime time)
+        {
+            // Called each logic update.
+            // As an example, we'll use the F5 key to make the game full screen
+            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.F5))
+            {
+                SadConsole.Settings.ToggleFullScreen();
+            }
+
+            // Keyboard movement for the player character: North
+            if (SadConsole.Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.D8))
+                {
+                    player.Position += new Point(0, -1);
+                }
+            // Keyboard movement for the player character: Northeast
+            if (SadConsole.Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.D9))
+                {
+                    player.Position += new Point(1, -1);
+                }
+            // Keyboard movement for the player character: East
+            if (SadConsole.Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.D6))
+                {
+                    player.Position += new Point(1, 0);
+                }
+            // Keyboard movement for the player character: Southeast
+            if (SadConsole.Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.D3))
+                {
+                    player.Position += new Point(1, 1);
+                }
+            // Keyboard movement for the player character: South
+            if (SadConsole.Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.D2))
+                {
+                    player.Position += new Point(0, 1);
+                }
+            // Keyboard movement for the player character: Southwest
+            if (SadConsole.Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.D1))
+                {
+                    player.Position += new Point(-1, 1);
+                }
+            // Keyboard movement for the player character: West
+            if (SadConsole.Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.D4))
+                {
+                    player.Position += new Point(-1, 0);
+                }
+            // Keyboard movement for the player character: Northwest
+            if (SadConsole.Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.D7))
+                {
+                    player.Position += new Point(-1, -1);
+                }
+
+        }
+
+        private static void Init()
+        {
+            // Any custom loading and preparation. We will use a sample console for now
+
+            Console startingConsole = new Console(Width, Height);
+            startingConsole.Fill(new Rectangle(3, 3, 27, 5), null, Color.Black, 0, SpriteEffects.None);
+            startingConsole.Print(6, 5, "Hello world!", ColorAnsi.CyanBright);
+
+            // Set our new console as the thing to render and process
+            SadConsole.Global.CurrentScreen = startingConsole;
+
+            // Create an instance of the player
+            CreatePlayer();
+
+            // Add the player Entity to our only console.
+            startingConsole.Children.Add(player);
+        }
+
+        // Create a player using Sadconsole's Entity class.
+        private static void CreatePlayer()
+        {
+            player = new SadConsole.Entities.Entity(1, 1);
+            player.Animation.CurrentFrame[0].Glyph = '@';
+            player.Animation.CurrentFrame[0].Foreground = Color.HotPink;
+            player.Position = new Point(20, 10);
+        }
+    }
+}
