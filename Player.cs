127c2f61df06da654b38def5acb28a2dc5f5d14b
Alexander Taxter <sharquo@gmail.com>
2019-11-07 20:32:49 +0000

    Missed another file.

---
+++ Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace TotD
+{
+    public class Player : Actor
+    {
+        public Player(Color foreground, Color background) : base(foreground, background, '@')
+        {
+
+        }
+    }
+}
